3743316de1af93215387f19cb5c30c6c667da592
Artem <artemzhdanov1299@gmail.com>
2023-02-06 00:38:14 +0200

    Add project files.

---
+++ lab2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exes1
+            //int[] mas = new int[10];
+            //Random rand = new Random();
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    mas[i] = rand.Next(-10, 10);
+            //    Console.WriteLine(mas[i]);
+            //}
+            //Console.WriteLine("\t");
+            //int chet = 0, nechet = 0, unik = 0;
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    if (mas[i] % 2 == 0)
+            //    {
+            //        chet++;
+            //    }
+            //    else if (mas[i] % 2 != 0)
+            //    {
+            //        nechet++;
+            //    }
+            //}
+            //int number = 0, count = 0;
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    number = mas[i];
+            //    for (int j = 0; j < mas.Length; j++)
+            //    {
+            //        if (number != mas[j]) { count++; }
+            //    }
+            //    if (count > 9) { unik++; }
+            //}
+            //Console.WriteLine("\t");
+            //Console.WriteLine($"Chet: {chet}\nNechet: {nechet}\tUnik: {unik}");
+            #endregion
+            #region Exes2
+            //int[] mas = new int[10];
+            //Random rand = new Random();
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    mas[i] = rand.Next(0, 100);
+            //}
+            //Console.WriteLine("Enter num 0-100");
+            //string n = Console.ReadLine();
+            //int num = Convert.ToInt32(n);
+            //int count = 0;
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    if (num > mas[i])
+            //    {
+            //        count++;
+            //    }
+            //    Console.Write($"{mas[i]}  ");
+            //}
+            //Console.WriteLine($"\nSumm= {count}");
+            #endregion
+
+            //#region Exes3
+            //int[] mas = new int[10];
+            //Random rand = new Random();
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    mas[i] = rand.Next(1, 10);
+            //}
+
+            //Console.WriteLine("Enter 3 number:");
+            //string nu1 = Console.ReadLine();
+            //int num1 = Convert.ToInt32(nu1);
+            //string nu2 = Console.ReadLine();
+            //int num2 = Convert.ToInt32(nu2);
+            //string nu3 = Console.ReadLine();
+            //int num3 = Convert.ToInt32(nu3);
+
+            //Console.WriteLine("\n");
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    Console.Write($"{mas[i]}  ");
+            //}
+
+            //int result = 0;
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    if (num1 == mas[i])
+            //    {
+            //        if (num2 == mas[i + 1])
+            //        {
+            //            if (num3 == mas[i + 2])
+            //            {
+            //                result++;
+            //            }
+            //        }
+            //    }
+            //}
+            //Console.WriteLine($"Result = {result}");
+            //#endregion
+        }
+    }
+}
